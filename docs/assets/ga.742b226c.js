002265a2ad93e918210afcac4d080e37b5176d43
Jack McGuire <jackmcguire35@gmail.com>
2023-03-03 11:38:50 +0000

    feat: hotjar and deps
    - update deps
    - introduce hotjar

---
+++ docs/assets/ga.742b226c.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index.33dd8135.js";import{i as o}from"./vue-gtag.esm.3e4dc491.js";var s=r(async({app:a,router:e,store:i})=>{a.use(o,{config:{id:"G-35F4WN4VX7",appName:"portfolio-website",pageTrackerScreenviewEnabled:!0,params:{anonymize_ip:!0}}},e)});export{s as default};
